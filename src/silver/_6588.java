4648d4a59962a6d3245c49aff7e7a0877e44f173
kho903 <gmldnr2222@naver.com>
2021-10-09 17:16:20 +0900

    백준 6588

---
+++ src/silver/_6588.java
@@ -0,0 +1,33 @@
+package silver;
+
+import java.util.*;
+
+public class _6588 {
+    public static final int MAX = 1000000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean[] check = new boolean[MAX + 1];
+        ArrayList<Integer> prime = new ArrayList<>();
+        check[0] = check[1] = true;
+        for (int i = 2; i * i <= MAX; i++) {
+            if (check[i])
+                continue;
+            prime.add(i);
+            for (int j = i + i; j <= MAX; j += i)
+                check[j] = true;
+        }
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0)
+                break;
+            for (int i = 1; i < prime.size(); i++) {
+                int p = prime.get(i);
+                if (!check[n - p]) {
+                    System.out.println(n + " = " + p + " + " + (n - p));
+                    break;
+                }
+            }
+        }
+    }
+}
