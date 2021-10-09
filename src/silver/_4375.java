f8ce93a0e2ded8c38f16b35ad251125513b8e9dc
kho903 <gmldnr2222@naver.com>
2021-10-09 14:59:14 +0900

    백준 4375

---
+++ src/silver/_4375.java
@@ -0,0 +1,21 @@
+package silver;
+
+import java.util.*;
+
+public class _4375 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int num = 0;
+            for (int i = 1; ; i++) {
+                num = num * 10 + 1;
+                num %= n;
+                if (num == 0) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+    }
+}
