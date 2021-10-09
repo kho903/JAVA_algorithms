a5cec880d4f74a696717a363ca4b5e86917abc94
kho903 <gmldnr2222@naver.com>
2021-10-09 15:35:45 +0900

    백준 17427

---
+++ src/silver/_17427.java
@@ -0,0 +1,15 @@
+package silver;
+
+import java.util.Scanner;
+
+public class _17427 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans += (n / i) * i;
+        }
+        System.out.println(ans);
+    }
+}
